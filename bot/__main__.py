b6ee6cb895ae6b86beb8d581604e398cc401ece1
Matias Barrientos <matias.barrientosg@gmail.com>
2020-06-27 03:45:58 -0400

    Creacion comando musica

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,35 +1,37 @@
 import discord
-from bot import private, error_handler, wolfram
+from bot import private, error_handler, wolfram, music
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='l:', description='Lucy Gühiart')
+
+class Bot(commands.Bot):
+
+    async def on_ready(self):
+        for guild in bot.guilds:
+            text_channel = [text for text in guild.text_channels if text.name.startswith('lprivate-')]
+
+            if text_channel:
+                for channel in text_channel:
+                    await channel.delete()
+
+            voice_channel = [voice for voice in guild.voice_channels if voice.name.startswith('lprivate-')]
+
+            if voice_channel:
+                for channel in voice_channel:
+                    await channel.delete()
+
+            roles = [role for role in guild.roles if role.name.startswith('lprivate-')]
+
+            if voice_channel:
+                for role in roles:
+                    await role.delete()
+
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="a mi general 😍"))
 
 
-@bot.event
-async def on_ready():
-    for guild in bot.guilds:
-        text_channel = [text for text in guild.text_channels if text.name.startswith('lprivate-')]
-
-        if text_channel:
-            for channel in text_channel:
-                await channel.delete()
-
-        voice_channel = [voice for voice in guild.voice_channels if voice.name.startswith('lprivate-')]
-
-        if voice_channel:
-            for channel in voice_channel:
-                await channel.delete()
-
-        roles = [role for role in guild.roles if role.name.startswith('lprivate-')]
-
-        if voice_channel:
-            for role in roles:
-                await role.delete()
-
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="a mi general 😍"))
-
+bot = Bot(command_prefix='l:', description='Lucy Gühiart')
 
 bot.add_cog(error_handler.CommandErrorHandler(bot))
 bot.add_cog(private.Private(bot))
 bot.add_cog(wolfram.Wolfram(bot))
+bot.add_cog(music.Music(bot))
 bot.run('NzI1MjQyOTMyMzkwNTI2OTk2.XvNmVg.dB42G_Tqne-bIzPZxUsjmFXzteo')
