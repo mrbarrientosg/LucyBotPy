88c024bbd26d63864d2c6e0304b52f1b999a4d1c
Matias Barrientos <matias.barrientosg@gmail.com>
2020-06-29 22:21:44 -0400

    Solucion problema commandos

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,67 +3,64 @@ from bot import private, error_handler, wolfram, music, copy_command
 from discord.ext import commands
 from discord.utils import get
 
+bot = commands.Bot(command_prefix='l:', description='Lucy Gühiart')
 
-class Bot(commands.Bot):
+@bot.event
+async def on_ready():
+    for guild in bot.guilds:
+        text_channel = [text for text in guild.text_channels if text.name.startswith('lprivate-')]
 
-    async def on_ready(self):
-        for guild in bot.guilds:
-            text_channel = [text for text in guild.text_channels if text.name.startswith('lprivate-')]
+        if text_channel:
+            for channel in text_channel:
+                await channel.delete()
 
-            if text_channel:
-                for channel in text_channel:
-                    await channel.delete()
+        voice_channel = [voice for voice in guild.voice_channels if voice.name.startswith('lprivate-')]
 
-            voice_channel = [voice for voice in guild.voice_channels if voice.name.startswith('lprivate-')]
+        if voice_channel:
+            for channel in voice_channel:
+                await channel.delete()
 
-            if voice_channel:
-                for channel in voice_channel:
-                    await channel.delete()
+        roles = [role for role in guild.roles if role.name.startswith('lprivate-')]
 
-            roles = [role for role in guild.roles if role.name.startswith('lprivate-')]
+        if roles:
+            for role in roles:
+                await role.delete()
 
-            if roles:
-                for role in roles:
-                    await role.delete()
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="a mi general 😍"))
 
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="a mi general 😍"))
+@bot.event
+async def on_member_join(member):
+    rol = get(member.guild.roles, name="Nuevo")
+    await member.add_roles(rol)
+    channel = get(member.guild.text_channels, name="bienvenido")
+    await channel.send('Holi {} de ahora me puedes llamar Lucy ❤️, debes elegir si ser procrastinador, socialista '
+                       'o ambos, enviame !procrastinador o !socialista, y si quieres ser ambos enviames ambos o '
+                       'es muy tonto lo que digo 🥰 ❤️'.format(member.mention))
 
-    async def on_member_join(self, member):
-        rol = get(member.guild.roles, name="Nuevo")
-        await member.add_roles(rol)
-        channel = get(member.guild.text_channels, name="bienvenido")
-        await channel.send('Holi {} de ahora me puedes llamar Lucy ❤️, debes elegir si ser procrastinador, socialista '
-                           'o ambos, enviame !procrastinador o !socialista, y si quieres ser ambos enviames ambos o '
-                           'es muy tonto lo que digo 🥰 ❤️'.format(member.mention))
+@bot.event
+async def on_message(message):
+    rol_nuevo = get(message.author.roles, name="Nuevo")
 
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
+    if message.channel.name == "bienvenido" and rol_nuevo:
+        soc = '!socialista' in message.content.lower()
+        pro = '!procrastinador' in message.content.lower()
 
-        rol_nuevo = get(message.author.roles, name="Nuevo")
+        if soc:
+            rol = get(message.author.guild.roles, name="Socialista")
+            await message.author.add_roles(rol)
 
-        if message.channel.name == "bienvenido" and rol_nuevo:
-            soc = '!socialista' in message.content.lower()
-            pro = '!procrastinador' in message.content.lower()
+        if pro:
+            rol = get(message.author.guild.roles, name="Procrastinador")
+            await message.author.add_roles(rol)
 
-            if soc:
-                rol = get(message.author.guild.roles, name="Socialista")
-                await message.author.add_roles(rol)
+        if soc or pro:
+            await message.author.remove_roles(rol_nuevo)
 
-            if pro:
-                rol = get(message.author.guild.roles, name="Procrastinador")
-                await message.author.add_roles(rol)
+        if not soc and not pro:
+            await message.channel.send(
+                '{} amoroso debes decirme si ser !procrastinador o !socialista ❤️'.format(message.author.mention))
 
-            if soc or pro:
-                await message.author.remove_roles(rol_nuevo)
-
-            if not soc and not pro:
-                await message.channel.send(
-                    '{} amoroso debes decirme si ser !procrastinador o !socialista ❤️'.format(message.author.mention))
-
-
-bot = Bot(command_prefix='l:', description='Lucy Gühiart')
+    await bot.process_commands(message)
 
 bot.add_cog(error_handler.CommandErrorHandler(bot))
 bot.add_cog(private.Private(bot))
